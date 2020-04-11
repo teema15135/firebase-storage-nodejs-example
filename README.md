2c2c129de36f1f83c226f16102354a53dd9b6e10
Teema Luksameset <teema.ma.luk@kkumail.com>
2020-04-11 13:15:30 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Firebase Storage Example with node js
+
+After clone the project, installed dependencies using
+```
+npm install
+```
+
+#
+Start server
+```
+npm start
+```
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
